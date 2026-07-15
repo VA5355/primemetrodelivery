7821de0c8ba2782a783561a725cd87b8241ac72e
Vinayak Anvekar <fairvinay@gmail.com>
2026-07-15 08:56:04 +0530

    added the title and favicon.ico for replace Next.js

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+	  <head>
+               <title>Prime Computer Retail</title>
+            <link rel="icon" href="/favicon/favicon.ico" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            </head>
       <body className="font-bodyFont dark:bg-gray-900">
        <Providers>
         <Header/>
